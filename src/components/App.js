b4856c797451036bb6001035e01ab00634d11faf
Feridun Canselen <canselenferidun@gmail.com>
2022-03-06 13:29:46 +0200

    structure of the app is done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import TopBar from "./Topbar";
 import "../styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <TopBar/>
     </div>
   );
 }
