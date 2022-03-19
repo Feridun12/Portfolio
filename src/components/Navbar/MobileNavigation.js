5c2878068e9d45e128f4ce04669003ea42f120a2
Feridun Canselen <canselenferidun@gmail.com>
2022-03-19 15:12:17 +0200

    added opening and closing button for mobile navbar

--- src/components/Navbar/MobileNavigation.js
+++ src/components/Navbar/MobileNavigation.js
@@ -2,16 +2,33 @@ import React from "react";
 import NavLinks from "./NavLinks";
 import "../Navbar/styles/TopBar.scss";
 import { CgMenu } from "react-icons/cg";
-import { useState } from 'react';
+import { CgCloseO } from "react-icons/cg";
+import { useState } from "react";
 
 function MobileNavigation() {
-const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const hamburgerIcon = (
+    <CgMenu
+      className="hamburger"
+      size="40px"
+      color="black"
+      onClick={() => setOpen(!open)}
+    />
+  );
+
+  const closeIcon = (
+    <CgCloseO
+      className="hamburger"
+      size="40px"
+      color="black"
+      onClick={() => setOpen(!open)}
+    />
+  );
 
   return (
     <nav className="mobile-navigation">
-      <CgMenu className="hamburger" size="40px" color="black"
-      onClick={() => setOpen(!open) } />
-      { open && <NavLinks />}
+      {open ? closeIcon : hamburgerIcon}
+      {open && <NavLinks />}
     </nav>
   );
 }
