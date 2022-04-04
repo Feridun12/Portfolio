f1f127ea3dd5b421cefc560c0401ce324c508244
Feridun Canselen <canselenferidun@gmail.com>
2022-04-04 13:45:18 +0300

    added a background image for intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,8 @@ import "../styles/Intro.scss";
 function Intro () {
     return (
         <div className="intro" id="intro">
-            Hello</div>
+            <h1>Feridun Djanselen</h1>
+        </div>
     )
 }
 
